49e8d7459152d5cfbb3c80c80443048182804390
Felipe Vieira <flpchapola@hotmail.com>
2017-09-28 22:58:08 -0300

    editado paginas iniciais, corrigido js de renderizacao da versao mobile, colocado onesignal id no arquivo de configuracao,feito as imagens de fundo e de logo

---
+++ www/js/index.js
@@ -0,0 +1,85 @@
+var app = {
+	// Application Constructor
+	initialize: function () {
+		document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
+		document.addEventListener('deviceready', function () {
+			// Enable to debug issues.
+			// window.plugins.OneSignal.setLogLevel({logLevel: 4, visualLevel: 4});
+
+			var notificationOpenedCallback = function (jsonData) {
+				console.log('notificationOpenedCallback: ' + JSON.stringify(jsonData));
+			};
+
+			window.plugins.OneSignal
+				.startInit("63f7ea5b-d2b3-4775-9039-ea7f022650b7")
+				.handleNotificationOpened(notificationOpenedCallback)
+				.endInit();
+
+			window.plugins.OneSignal.getIds(function (ids) {
+				console.log('getIds: ' + JSON.stringify(ids));
+				console.log("userId = " + ids.userId + ", pushToken = " + ids.pushToken);
+				// alert("userId = " + ids.userId + ", pushToken = " + ids.pushToken);
+
+			});
+
+			// Call syncHashedEmail anywhere in your app if you have the user's email.
+			// This improves the effectiveness of OneSignal's "best-time" notification scheduling feature.
+			// window.plugins.OneSignal.syncHashedEmail(userEmail);
+		}, false);
+	},
+
+	// deviceready Event Handler
+	//
+	// Bind any cordova events here. Common events are:
+	// 'pause', 'resume', etc.
+	onDeviceReady: function () {
+		this.receivedEvent('deviceready');
+
+		document.getElementById("abrirApp")
+			.addEventListener("click", abrirApp, false);
+
+	},
+
+	// Update DOM on a Received Event
+	receivedEvent: function (id) {
+		console.log('Received Event: ' + id);
+	},
+};
+
+function getAjax(url, success) {
+	var xhr = window.XMLHttpRequest ? new XMLHttpRequest() : new ActiveXObject('Microsoft.XMLHTTP');
+	xhr.open('GET', url);
+	xhr.onreadystatechange = function () {
+		if (xhr.readyState > 3 && xhr.status == 200) success(xhr.responseText);
+	};
+	xhr.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
+	xhr.send();
+	return xhr;
+}
+
+function postAjax(url, success) {
+	var xhr = window.XMLHttpRequest ? new XMLHttpRequest() : new ActiveXObject('Microsoft.XMLHTTP');
+	xhr.open('POST', url);
+	xhr.onreadystatechange = function () {
+		if (xhr.readyState > 3 && xhr.status == 200) success(xhr.responseText);
+	};
+	xhr.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
+	xhr.send();
+	return xhr;
+}
+
+function onError(error) {
+	alert("code: " + error.code +
+		"message:" + error.message);
+}
+
+function abrirApp() {
+	document.getElementById("loader-pre-container").classList.add("hide-loader");
+	var pushID = document.getElementById("OneSignalUserID").textContent;
+	var pushToken = document.getElementById("OneSignalPushToken").textContent;
+	var urlRedirect = 'http://app.meupersonalvirtual.com.br';
+	var urlRedirectFinal = urlRedirect + "&appUserId=" + pushID + "&appPushToken=" + pushToken;
+	window.open(urlRedirectFinal, "_self");
+}
+
+app.initialize();
